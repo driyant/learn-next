3b453567519f994ccb51784199cc187305051756
driyant <message.driyant@outlook.com>
2023-12-12 15:22:15 +0700

    fix: add config eslint

---
+++ pages/__tests__/index.js
@@ -0,0 +1,8 @@
+const { render, screen } = require("@testing-library/react");
+const { default: Home } = require("..");
+
+describe("component should render properly", () => {
+  it("should return true", () => {
+    expect(true).toBe(true);
+  });
+});
