827e94767a791f3885d60cf618ea37c662b7f1c3
driyant <message.driyant@outlook.com>
2024-01-08 14:54:28 +0700

    feat: DT-13

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Input, Textarea, Button } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { DeleteIcon, AddIcon, EditIcon } from "@chakra-ui/icons";
@@ -10,8 +10,54 @@ const DynamicLayout = dynamic(() => import("@/layout"), {
 
 const NoteEdit = () => {
   const router = useRouter();
-  const { id } = router.query;
-  console.log(id);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const fetchNote = async () => {
+    try {
+      const res = await fetch(
+        `https://paace-f178cafcae7b.nevacloud.io/api/notes/${router.query.id}`
+      );
+      const data = await res.json();
+      setTitle(data.data.title);
+      setDescription(data.data.description);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async () => {
+    const data = {
+      title,
+      description,
+    };
+    try {
+      const res = await fetch(
+        `https://paace-f178cafcae7b.nevacloud.io/api/notes/update/${router.query.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      console.log(res);
+      router.push("/notes");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(title);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchNote();
+    }, 1000);
+  }, []);
+
   return (
     <DynamicLayout>
       <Box
@@ -21,9 +67,20 @@ const NoteEdit = () => {
         justifyContent="space-evenly"
         flexFlow="column"
       >
-        <Input type="text" placeholder="Title" margin="1rem 0" />
-        <Textarea placeholder="Description " margin="1rem 0"></Textarea>
-        <Button>Update</Button>
+        <Input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          margin="1rem 0"
+        />
+        <Textarea
+          placeholder="Description "
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          margin="1rem 0"
+        ></Textarea>
+        <Button onClick={handleUpdate}>Update</Button>
       </Box>
     </DynamicLayout>
   );
